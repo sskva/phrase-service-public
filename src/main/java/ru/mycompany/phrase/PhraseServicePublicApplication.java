ef43ae93e635ab16c27dc1bd48ba4bdeffcfcbe2
ssk11 <ssk29@yandex.ru>
2022-10-30 12:43:12 +0300

    Initial commit

---
+++ src/main/java/ru/mycompany/phrase/PhraseServicePublicApplication.java
@@ -0,0 +1,13 @@
+package ru.mycompany.phrase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhraseServicePublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhraseServicePublicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mycompany/phrase/PhraseServicePublicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mycompany.phrase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhraseServicePublicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
